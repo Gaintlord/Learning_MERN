46e4144235321c4a0626e24c6e6c6fc58edacea3
kishor <kishor.jha243@gmail.com>
2025-02-01 20:43:50 +0530

    user validation check

--- routerByExpress/user_route.js
+++ routerByExpress/user_route.js
@@ -1,11 +1,35 @@
 const {Router}= require('express');
-
-
+const {z} = require('zod')
+const {usermodel} = require('../DataBase.js')
 const userRouter = Router();
 
-userRouter.get('',(req,res)=>
-    res.send('okay check fine')
-    )
+userRouter.get('',async (req,res)=>
+
+{
+const validate = z.object({
+    email:z.string().email(),
+    password:z.string().min(8),
+    FirstName:z.string(),
+    LastName:z.string()
+})
+
+try {
+validate.parse(req.body)    
+
+const {email,password,FirstName,LastName}=req.body
+usermodel.create({
+    email:email,
+    password:password,
+    FirstName:FirstName,
+    LastName:LastName
+})
+
+res.status(200).send("data is valid and entered")
+} catch (error) {
+    res.status(400).send("nikal jayo madherchod")
+}
+
+})
 
     userRouter.get('/signup',(req,res)=>{
         res.send('working as of now')
